9fd177f096b8bb209b529f5548366d1b49c5f80d
Dr-Zhi <yl.zhi@hotmail.com>
2013-03-23 16:52:04 +0800

    add reverse single linked list algorithm

---
+++ Algorithms/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Yanling Zhi on 3/23/13.
+//  Copyright (c) 2013 Dr. Zhi Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
